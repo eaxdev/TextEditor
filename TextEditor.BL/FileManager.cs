5e870c2da05bf6571fc5bac47e52edc25c4658df
roma-sk3 <roma-sk3@ya.ru>
2016-01-23 21:21:44 +0300

    add two methods: get and save contents

---
+++ TextEditor.BL/FileManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.IO;
+
+namespace TextEditor.BL
+{
+    public class FileManager
+    {
+        private readonly Encoding _defaultEncoding = Encoding.GetEncoding(1251);
+        public string GetContent(string filePath)
+        {
+            return GetContent(filePath, _defaultEncoding);
+        }
+        public string GetContent(string filePath, Encoding encoding)
+        {
+            string content = File.ReadAllText(filePath, encoding);
+            return content;
+        }
+
+        public void SaveContent(string content, string filePath, Encoding encoding)
+        {
+            File.WriteAllText(filePath, content, encoding);
+        }
+    }
+}
