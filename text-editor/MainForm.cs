9bbf5047723b28f731c8653968cdd0c5b2ee0853
roma-sk3 <roma-sk3@ya.ru>
2016-01-23 22:05:23 +0300

    add interface IMainForm

--- text-editor/MainForm.cs
+++ text-editor/MainForm.cs
@@ -10,11 +10,36 @@ using System.Windows.Forms;
 
 namespace text_editor
 {
-    public partial class MainForm : Form
+    public interface IMainForm
+    {
+        string FilePath { get; }
+        string Content { get; set; }
+        void SetSymbolCount(int count);
+        event EventHandler FileOpenClick;
+        event EventHandler FileSaveClick;
+        event EventHandler ContentChanged;
+    }
+    public partial class MainForm : Form, IMainForm
     {
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        public string FilePath
+        {
+            get { return fldFilePath.Text; } 
+            
+        }
+    
+        public string Content
+        {
+            get { return fldContent.Text; }
+            set { fldContent.Text = value; }
+        }
+        public void SetSymbolCount(int count)
+        {
+            throw new NotImplementedException();
         }
     }
 }
