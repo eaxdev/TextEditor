2bb19bedaf2b69e19ee5aea827c79f8792b8759f
roma-sk3 <roma-sk3@ya.ru>
2016-01-23 21:28:01 +0300

    add method IsExist

--- TextEditor.BL/FileManager.cs
+++ TextEditor.BL/FileManager.cs
@@ -6,6 +6,11 @@ namespace TextEditor.BL
     public class FileManager
     {
         private readonly Encoding _defaultEncoding = Encoding.GetEncoding(1251);
+        public bool IsExist(string filePath)
+        {
+            bool isExist = File.Exists(filePath);
+            return isExist;
+        }
         public string GetContent(string filePath)
         {
             return GetContent(filePath, _defaultEncoding);
@@ -15,7 +20,10 @@ namespace TextEditor.BL
             string content = File.ReadAllText(filePath, encoding);
             return content;
         }
-
+        public void SaveContent(string content, string filePath)
+        {
+            File.WriteAllText(filePath, content, _defaultEncoding);
+        }
         public void SaveContent(string content, string filePath, Encoding encoding)
         {
             File.WriteAllText(filePath, content, encoding);
