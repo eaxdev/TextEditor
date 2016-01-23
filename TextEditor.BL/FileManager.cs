ec046cc5471a313c3a03368ccacdc120a89a4b61
roma-sk3 <roma-sk3@ya.ru>
2016-01-23 21:29:44 +0300

    add method GetCount

--- TextEditor.BL/FileManager.cs
+++ TextEditor.BL/FileManager.cs
@@ -6,27 +6,38 @@ namespace TextEditor.BL
     public class FileManager
     {
         private readonly Encoding _defaultEncoding = Encoding.GetEncoding(1251);
+
         public bool IsExist(string filePath)
         {
             bool isExist = File.Exists(filePath);
             return isExist;
         }
+
         public string GetContent(string filePath)
         {
             return GetContent(filePath, _defaultEncoding);
         }
+
         public string GetContent(string filePath, Encoding encoding)
         {
             string content = File.ReadAllText(filePath, encoding);
             return content;
         }
+
         public void SaveContent(string content, string filePath)
         {
             File.WriteAllText(filePath, content, _defaultEncoding);
         }
+
         public void SaveContent(string content, string filePath, Encoding encoding)
         {
             File.WriteAllText(filePath, content, encoding);
+        }
+   
+        public int GetSymbolCount(string content)
+        {
+            int count = content.Length;
+            return count;
         }
     }
 }
